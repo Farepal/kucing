81c85d001e1f50d07ee95e95786d39f6fe93bf23
farreladriann <muhamadfarreladrian@mail.ugm.ac.id>
2024-09-04 21:53:08 +0700

    sekarang php bos

--- nand2tetris/projects/10/index.php
+++ nand2tetris/projects/10/index.php
@@ -5,18 +5,34 @@ class JackTokenizer
     private $regexSymbol = '/^(\{|\}|\(|\)|\[|\]|\.|\,|\;|\+|\-|\*|\/|\&|\||\<|\>|\=|\~)$/';
     private $regexIntegerConstant = '/^(\d+)$/';
     private $regexStringConstant = '/^\"(.*)\"$/';
-    private $regexIdentifier = '/^([a-zA-Z_][a-zA-Z0-9]*)$/';
+    private $regexIdentifier = '/^([a-zA-Z_][a-zA-Z0-9_]*)$/';
 
-    private $outputFile;
     private $currentToken = '';
     private $tokens = [];
     private $currentIndex = 0;
 
     public function __construct($inputFile)
     {
-        $this->outputFile = str_replace('.jack', 'FT.xml', $inputFile);
         $allLine = file($inputFile, FILE_IGNORE_NEW_LINES);
         $this->tokens = $this->cleanCodeToTokens($allLine);
+    }
+
+    public function getCurrentToken()
+    {
+        switch ($this->tokenType()) {
+            case 'KEYWORD':
+                return $this->keyword();
+            case 'SYMBOL':
+                return $this->symbol();
+            case 'IDENTIFIER':
+                return $this->identifier();
+            case 'INT_CONST':
+                return $this->intVal();
+            case 'STRING_CONST':
+                return $this->stringVal();
+            default:
+                return 'UNKNOWN';
+        }
     }
 
     public function hasMoreTokens()
@@ -110,26 +126,397 @@ class JackTokenizer
     }
 }
 
+class compilationEngine
+{
+    private $nextToken, $nextTokenType;
+    private $numberOfTabs = 0;
+    private $tokenizer;
+    private $outputFileXML, $outputFileTXML;
+
+    public function __construct($inputFile)
+    {
+        $this->outputFileXML = str_replace('.jack', 'php.xml', $inputFile);
+        file_put_contents($this->outputFileXML, '');
+        $this->outputFileTXML = str_replace('.jack', 'phpT.xml', $inputFile);
+        file_put_contents($this->outputFileTXML, '');
+        $this->tokenizer = new JackTokenizer($inputFile);
+        $this->fullProcess();
+    }
+
+    private function advance()
+    {
+        $this->writeCompilationEngine($this->getXMLdata());
+        $this->tokenizerAdvanceAndWriteT();
+    }
+
+    private function tokenizerAdvanceAndWriteT()
+    {
+        if ($this->tokenizer->hasMoreTokens()) {
+            $this->tokenizer->advance();
+            $this->nextToken = $this->tokenizer->getCurrentToken();
+            $this->nextTokenType = $this->tokenizer->tokenType();
+            file_put_contents($this->outputFileTXML, $this->getXMLdata() . "\n", FILE_APPEND);
+        }
+    }
+
+    private function fullProcess()
+    {
+        file_put_contents($this->outputFileTXML, "<tokens>\n", FILE_APPEND);
+        $this->tokenizerAdvanceAndWriteT();
+        $this->compileClass();
+        file_put_contents($this->outputFileTXML, "</tokens>\n", FILE_APPEND);
+    }
+
+    private function getXMLdata()
+    {
+        $tokenTypeParser = [
+            'KEYWORD' => 'keyword',
+            'SYMBOL' => 'symbol',
+            'IDENTIFIER' => 'identifier',
+            'INT_CONST' => 'integerConstant',
+            'STRING_CONST' => 'stringConstant',
+        ];
+
+        $xmlData = "<{$tokenTypeParser[$this->nextTokenType]}> {$this->nextToken} </{$tokenTypeParser[$this->nextTokenType]}>";
+        return $xmlData;
+    }
+
+    private function writeCompilationEngine($string)
+    {
+        $tabs = str_repeat('  ', $this->numberOfTabs);
+        $stringWithTabs = $tabs . $string;
+        file_put_contents($this->outputFileXML, $stringWithTabs . "\n", FILE_APPEND);
+    }
+
+    private function writeNonTerminalStart($tag)
+    {
+        $this->writeCompilationEngine("<$tag>");
+        $this->numberOfTabs += 1;
+    }
+
+    private function writeNonTerminalEnd($tag)
+    {
+        $this->numberOfTabs -= 1;
+        $this->writeCompilationEngine("</$tag>");
+    }
+
+    private function lookAheadToken($token)
+    {
+        return $this->nextToken == $token;
+    }
+
+    private function lookAheadTokenType($tokenType)
+    {
+        return $this->nextTokenType == $tokenType;
+    }
+
+    private function isNextStatement()
+    {
+        return $this->lookAheadToken('let') || $this->lookAheadToken('if') || $this->lookAheadToken('while') || $this->lookAheadToken('do') || $this->lookAheadToken('return');
+    }
+
+    private function isNextOperator()
+    {
+        return $this->lookAheadToken('+') || $this->lookAheadToken('-') || $this->lookAheadToken('*') || $this->lookAheadToken('/') || $this->lookAheadToken('&amp;') || $this->lookAheadToken('|') || $this->lookAheadToken('&lt;') || $this->lookAheadToken('&gt;') || $this->lookAheadToken('=');
+    }
+
+    private function isNextUnaryOperator()
+    {
+        return $this->lookAheadToken('-') || $this->lookAheadToken('~');
+    }
+
+    private function isNextKeywordConstant()
+    {
+        return $this->lookAheadToken('true') || $this->lookAheadToken('false') || $this->lookAheadToken('null') || $this->lookAheadToken('this');
+    }
+
+    private function compileClass()
+    {
+        $this->writeNonTerminalStart('class');
+        $this->advance(); // class
+        $this->advance(); // className
+        $this->advance(); // {
+        while ($this->lookAheadToken('static') || $this->lookAheadToken('field')) {
+            $this->compileClassVarDec();
+        }
+        while ($this->lookAheadToken('constructor') || $this->lookAheadToken('function') || $this->lookAheadToken('method')) {
+            $this->compileSubroutine();
+        }
+        $this->advance(); // }
+        $this->writeNonTerminalEnd('class');
+    }
+
+    private function compileClassVarDec()
+    {
+        $this->writeNonTerminalStart('classVarDec');
+        $this->advance(); // static | field
+        $this->advance(); // type
+        $this->advance(); // varName
+        while ($this->lookAheadToken(',')) {
+            $this->advance(); // ,
+            $this->advance(); // varName
+        }
+        $this->advance(); // ;
+        $this->writeNonTerminalEnd('classVarDec');
+    }
+
+    private function compileSubroutine()
+    {
+        $this->writeNonTerminalStart('subroutineDec');
+        $this->advance(); // constructor | function | method
+        $this->advance(); // void | type
+        $this->advance(); // subroutineName
+        $this->advance(); // (
+        $this->compileParameterList();
+        $this->advance(); // )
+        $this->compileSubroutineBody();
+        $this->writeNonTerminalEnd('subroutineDec');
+    }
+
+    private function compileParameterList()
+    {
+        $this->writeNonTerminalStart('parameterList');
+        if (!$this->lookAheadToken(')')) {
+            $this->advance(); // type
+            $this->advance(); // varName
+            while ($this->lookAheadToken(',')) {
+                $this->advance(); // ,
+                $this->advance(); // type
+                $this->advance(); // varName
+            }
+        }
+        $this->writeNonTerminalEnd('parameterList');
+    }
+
+    private function compileSubroutineBody()
+    {
+        $this->writeNonTerminalStart('subroutineBody');
+        $this->advance(); // {
+        while ($this->lookAheadToken('var')) {
+            $this->compileVarDec();
+        }
+        while ($this->isNextStatement()) {
+            $this->compileStatements();
+        }
+        $this->advance(); // }
+        $this->writeNonTerminalEnd('subroutineBody');
+    }
+
+    private function compileVarDec()
+    {
+        $this->writeNonTerminalStart('varDec');
+        $this->advance(); // var
+        $this->advance(); // type
+        $this->advance(); // varName
+        while ($this->lookAheadToken(',')) {
+            $this->advance(); // ,
+            $this->advance(); // varName
+        }
+        $this->advance(); // ;
+        $this->writeNonTerminalEnd('varDec');
+    }
+
+    private function compileStatements()
+    {
+        $this->writeNonTerminalStart('statements');
+        while ($this->isNextStatement()) {
+            if ($this->lookAheadToken('let')) {
+                $this->compileLet();
+            } elseif ($this->lookAheadToken('if')) {
+                $this->compileIf();
+            } elseif ($this->lookAheadToken('while')) {
+                $this->compileWhile();
+            } elseif ($this->lookAheadToken('do')) {
+                $this->compileDo();
+            } elseif ($this->lookAheadToken('return')) {
+                $this->compileReturn();
+            }
+        }
+        $this->writeNonTerminalEnd('statements');
+    }
+
+    private function compileLet()
+    {
+        $this->writeNonTerminalStart('letStatement');
+        $this->advance(); // let
+        $this->advance(); // varName
+        if ($this->lookAheadToken('[')) {
+            $this->advance(); // [
+            $this->compileExpression();
+            $this->advance(); // ]
+        }
+        $this->advance(); // =
+        $this->compileExpression();
+        $this->advance(); // ;
+        $this->writeNonTerminalEnd('letStatement');
+    }
+
+    private function compileIf()
+    {
+        $this->writeNonTerminalStart('ifStatement');
+        $this->advance(); // if
+        $this->advance(); // (
+        $this->compileExpression();
+        $this->advance(); // )
+        $this->advance(); // {
+        $this->compileStatements();
+        while ($this->isNextStatement()) {
+            $this->compileStatements();
+        }
+        $this->advance(); // }
+        if ($this->lookAheadToken('else')) {
+            $this->advance(); // else
+            $this->advance(); // {
+            $this->compileStatements();
+            while ($this->isNextStatement()) {
+                $this->compileStatements();
+            }
+            $this->advance(); // }
+        }
+        $this->writeNonTerminalEnd('ifStatement');
+    }
+
+    private function compileWhile()
+    {
+        $this->writeNonTerminalStart('whileStatement');
+        $this->advance(); // while
+        $this->advance(); // (
+        $this->compileExpression();
+        $this->advance(); // )
+        $this->advance(); // {
+        $this->compileStatements();
+        while ($this->isNextStatement()) {
+            $this->compileStatements();
+        }
+        $this->advance(); // }
+        $this->writeNonTerminalEnd('whileStatement');
+    }
+
+    private function compileDo()
+    {
+        $this->writeNonTerminalStart('doStatement');
+        $this->advance(); // do
+        $this->advance(); // subroutineName | className | varName
+        while ($this->lookAheadToken('.') || $this->lookAheadToken('(')) {
+            if ($this->lookAheadToken('.')) {
+                $this->advance(); // .
+                $this->advance(); // subroutineName
+                $this->advance(); // (
+                $this->compileExpressionList();
+                $this->advance(); // )
+            } elseif ($this->lookAheadToken('(')) {
+                $this->advance(); // (
+                $this->compileExpressionList();
+                $this->advance(); // )
+            }
+        }
+        $this->advance(); // ;
+        $this->writeNonTerminalEnd('doStatement');
+    }
+
+    private function compileReturn()
+    {
+        $this->writeNonTerminalStart('returnStatement');
+        $this->advance(); // return
+        if (!$this->lookAheadToken(';')) {
+            $this->compileExpression();
+        }
+        $this->advance(); // ;
+        $this->writeNonTerminalEnd('returnStatement');
+    }
+
+    private function compileExpression()
+    {
+        $this->writeNonTerminalStart('expression');
+        $this->compileTerm();
+        while ($this->isNextOperator()) {
+            $this->advance(); // operator
+            $this->compileTerm();
+        }
+        $this->writeNonTerminalEnd('expression');
+    }
+
+    private function compileTerm()
+    {
+        $this->writeNonTerminalStart('term');
+        if ($this->lookAheadTokenType('INT_CONST') || $this->lookAheadTokenType('STRING_CONST') || $this->isNextKeywordConstant()) {
+            $this->advance(); // intConst | stringConst | keywordConstant
+        } elseif ($this->lookAheadTokenType('IDENTIFIER')) {
+            $this->advance(); // varName
+            if ($this->lookAheadToken('[')) {
+                $this->advance(); // [
+                $this->compileExpression();
+                $this->advance(); // ]
+            } elseif ($this->lookAheadToken('.')) {
+                $this->advance(); // .
+                $this->advance(); // subroutineName
+                $this->advance(); // (
+                $this->compileExpressionList();
+                $this->advance(); // )
+            } elseif ($this->lookAheadToken('(')) {
+                $this->advance(); // (
+                $this->compileExpressionList();
+                $this->advance(); // )
+            }
+        } elseif ($this->isNextUnaryOperator()) {
+            $this->advance(); // unaryOp
+            $this->compileTerm();
+        } elseif ($this->lookAheadToken('(')) {
+            $this->advance(); // (
+            $this->compileExpression();
+            $this->advance(); // )
+        }
+        $this->writeNonTerminalEnd('term');
+    }
+
+    private function compileExpressionList()
+    {
+        $this->writeNonTerminalStart('expressionList');
+        if (!$this->lookAheadToken(')')) {
+            $this->compileExpression();
+            while ($this->lookAheadToken(',')) {
+                $this->advance(); // ,
+                $this->compileExpression();
+            }
+        }
+        $this->writeNonTerminalEnd('expressionList');
+    }
+}
+
+class jackAnalyzer
+{
+    public function __construct($inputFolderOrFile)
+    {
+        if (is_dir($inputFolderOrFile)) {
+            $files = glob($inputFolderOrFile . '/*.jack');
+            foreach ($files as $file) {
+                $this->analyze($file);
+            }
+        } elseif (is_file($inputFolderOrFile)) {
+            $this->analyze($inputFolderOrFile);
+        }
+    }
+
+    public function analyze($inputFile)
+    {
+        $compilationEngine = new CompilationEngine($inputFile);
+    }
+}
+
 function main($argv)
 {
     if (isset($argv[1])) {
-        $inputFile = $argv[1];
-        $tokenizer = new JackTokenizer($inputFile);
-        while ($tokenizer->hasMoreTokens()) {
-            $tokenizer->advance();
-            $tokenType = $tokenizer->tokenType();
-            if ($tokenType == 'KEYWORD') {
-                echo "<keyword> " . $tokenizer->keyword() . " </keyword>\n";
-            } elseif ($tokenType == 'SYMBOL') {
-                echo "<symbol> " . $tokenizer->symbol() . " </symbol>\n";
-            } elseif ($tokenType == 'IDENTIFIER') {
-                echo "<identifier> " . $tokenizer->identifier() . " </identifier>\n";
-            } elseif ($tokenType == 'INT_CONST') {
-                echo "<integerConstant> " . $tokenizer->intVal() . " </integerConstant>\n";
-            } elseif ($tokenType == 'STRING_CONST') {
-                echo "<stringConstant> " . $tokenizer->stringVal() . " </stringConstant>\n";
-            }
-        }
+        $inputFolderOrFile = $argv[1];
+        $jackAnalyzer = new JackAnalyzer($inputFolderOrFile);
+        // $jack
+        // $jackTokenizer = new JackTokenizer($inputFolderOrFile);
+        // $count = 0;
+        // while ($jackTokenizer->hasMoreTokens()) {
+        // $jackTokenizer->advance();
+        // echo $jackTokenizer->getCurrentToken() . $jackTokenizer->tokenType() . "\n";
+        // $count++;
+        // }
+        // echo $count . "\n";
     } else {
         echo "Usage: php JackTokenizer.php <inputFile.jack>\n";
     }
--- nand2tetris/tools/compareAll.sh
+++ nand2tetris/tools/compareAll.sh
@@ -61,3 +61,68 @@ echo ""
 echo "Square/SquareGameT"
 sh TextComparer.sh ../projects/10/Square/SquareGameT.xml ../projects/10/Square/SquareGameFT.xml
 echo ""
+
+
+php ../projects/10/index.php ../projects/10/ArrayTest
+echo "php ../projects/10/index.php ../projects/10/ExpressionLessSquare"
+php ../projects/10/index.php ../projects/10/Square
+echo "php ../projects/10/index.php ../projects/10/ExpressionLessSquare"
+php ../projects/10/index.php ../projects/10/ExpressionLessSquare
+echo "php ../projects/10/index.php ../projects/10/Square"
+echo ""
+
+echo "ArrayTest/Main"
+sh TextComparer.sh ../projects/10/ArrayTest/Main.xml ../projects/10/ArrayTest/Mainphp.xml
+echo ""
+
+echo "ArrayTest/MainT"
+sh TextComparer.sh ../projects/10/ArrayTest/MainT.xml ../projects/10/ArrayTest/MainphpT.xml
+echo ""
+
+echo "ExpressionLessSquare/Square"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/Main.xml ../projects/10/ExpressionLessSquare/Mainphp.xml
+echo ""
+
+echo "ExpressionLessSquare/SquareT"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/MainT.xml ../projects/10/ExpressionLessSquare/MainphpT.xml
+echo ""
+
+echo "ExpressionLessSquare/Square"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/Square.xml ../projects/10/ExpressionLessSquare/Squarephp.xml
+echo ""
+
+echo "ExpressionLessSquare/SquareT"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/SquareT.xml ../projects/10/ExpressionLessSquare/SquarephpT.xml
+echo ""
+
+echo "ExpressionLessSquare/SquareGame"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/SquareGame.xml ../projects/10/ExpressionLessSquare/SquareGamephp.xml
+echo ""
+
+echo "ExpressionLessSquare/SquareGameT"
+sh TextComparer.sh ../projects/10/ExpressionLessSquare/SquareGameT.xml ../projects/10/ExpressionLessSquare/SquareGamephpT.xml
+echo ""
+
+echo "Square/Main"
+sh TextComparer.sh ../projects/10/Square/Main.xml ../projects/10/Square/Mainphp.xml
+echo ""
+
+echo "Square/MainT"
+sh TextComparer.sh ../projects/10/Square/MainT.xml ../projects/10/Square/MainphpT.xml
+echo ""
+
+echo "Square/Square"
+sh TextComparer.sh ../projects/10/Square/Square.xml ../projects/10/Square/Squarephp.xml
+echo ""
+
+echo "Square/SquareT"
+sh TextComparer.sh ../projects/10/Square/SquareT.xml ../projects/10/Square/SquarephpT.xml
+echo ""
+
+echo "Square/SquareGame"
+sh TextComparer.sh ../projects/10/Square/SquareGame.xml ../projects/10/Square/SquareGamephp.xml
+echo ""
+
+echo "Square/SquareGameT"
+sh TextComparer.sh ../projects/10/Square/SquareGameT.xml ../projects/10/Square/SquareGamephpT.xml
+echo ""
